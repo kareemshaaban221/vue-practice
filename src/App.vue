7a2354e741e70fa232b00908a74ca9d4ab78209e
kareem.shaaban <kareem.shaaban@brightcreations.com>
2024-11-29 19:42:35 +0200

    [api]

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,16 @@
 <script setup>
 import HeadingOne from './components/partials/HeadingOne.vue';
 import TasksPage from './components/TasksPage.vue';
-import data from './tasks.json';
+import configs from "../config/api.json";
+// import data from './tasks.json';
 
-const tasks = data;
+const tasks = await getTasks();
+
+async function getTasks() {
+  const response = await fetch(configs.api.url);
+  const data = await response.json();
+  return data;
+}
 </script>
 
 <template>
